2034e1a5aae519db987c80eaef5b682568e7b784
Saidul Islam Santo <sksanto.pro@gmail.com>
2023-02-04 12:31:16 +0600

    signup form setup

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,32 +1,123 @@
-import { Link } from 'react-router-dom';
+/* eslint-disable no-unused-vars */
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import classes from '../Styles/Signup.module.css';
 import Button from './Button';
 import Checkbox from './Checkbox';
+import { useAuth } from './Contexts/AuthContext';
 import Form from './Form';
 import TextInput from './TextInput';
 
 export default function SignupForm() {
+	const [username, setUsername] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [confirmPassword, setConfirmPassword] = useState('');
+	const [agree, setAgree] = useState();
+
+	const [error, setError] = useState();
+	const [loading, setLoading] = useState();
+
+	const { signup } = useAuth();
+	const navigate = useNavigate();
+
+	async function handleSubmit(e) {
+		e.preventDefault();
+
+		//=> validition:
+		if (password !== confirmPassword) {
+			return setError(`Password don't match!`);
+		}
+
+		try {
+			setError('');
+			setLoading(true);
+			await signup(email, password, username);
+			navigate('/');
+		} catch (err) {
+			// console.log(err);
+			setLoading(false);
+
+			switch (err.code) {
+				case 'auth/weak-password':
+					setError('Password should be at least 6 characters');
+					break;
+				case 'auth/invalid-email':
+					setError('Invalid Email');
+					break;
+				case 'auth/email-already-in-use':
+					setError('The email address is already in use');
+					break;
+				case 'auth/operation-not-allowed':
+					setError('Operation not allowed');
+					break;
+				default:
+					setError('Faild to signup!!');
+					break;
+			}
+		}
+	}
+
 	return (
-		<Form className={`${classes.signup}`}>
-			<TextInput type="text" placeholder="Enter name" icon="person" />
+		<Form className={`${classes.signup}`} onSubmit={handleSubmit}>
+			<TextInput
+				type="text"
+				required
+				placeholder="Enter name"
+				icon="person"
+				value={username}
+				onChange={(e) => {
+					setUsername(e.target.value);
+				}}
+			/>
 
-			<TextInput type="text" placeholder="Enter eamil" icon="alternate_email" />
-
-			<TextInput type="password" placeholder="Enter password" icon="lock" />
+			<TextInput
+				type="text"
+				required
+				placeholder="Enter email"
+				icon="alternate_email"
+				value={email}
+				onChange={(e) => {
+					setEmail(e.target.value);
+				}}
+			/>
 
 			<TextInput
 				type="password"
-				placeholder="Confirm password"
-				icon="lock_clock"
+				required
+				placeholder="Enter password"
+				icon="lock"
+				value={password}
+				onChange={(e) => {
+					setPassword(e.target.value);
+				}}
 			/>
 
-			<TextInput type="text" placeholder="Enter name" icon="person" />
+			<TextInput
+				type="password"
+				required
+				placeholder="Confirm password"
+				icon="lock_clock"
+				value={confirmPassword}
+				onChange={(e) => {
+					setConfirmPassword(e.target.value);
+				}}
+			/>
 
-			<Checkbox text="I agree to the Terms &amp; Conditions" />
+			<Checkbox
+				text="I agree to the Terms &amp; Conditions"
+				required
+				value={agree}
+				onChange={(e) => {
+					setAgree(e.target.value);
+				}}
+			/>
 
-			<Button>
+			<Button type="submit">
 				<span>Submit now</span>
 			</Button>
+
+			{error && <p className="error">{error}</p>}
 
 			<div class="info">
 				Already have an account? <Link to="../login">Login</Link> instead.
