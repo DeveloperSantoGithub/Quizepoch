10d87cbbe75844a1596ae26b4bea0cef13cda188
Saidul Islam Santo <sksanto.pro@gmail.com>
2023-02-06 17:04:45 +0600

    update

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -22,7 +22,7 @@ export default function ProgressBar({ next, prev, submitQuiz, progress }) {
 					className={classes.next}
 					onClick={progress === 100 ? submitQuiz : next}
 				>
-					<span>Next Question</span>
+					<span>{progress === 100 ? 'Submit Quiz' : 'Next Question'}</span>
 					<span className="material-icons-outlined"> arrow_forward </span>
 				</Button>
 			</div>
