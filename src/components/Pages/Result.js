9f95d4290ef8e239dd72e9961c3aa8fdf447c7e0
Saidul Islam Santo <sksanto.pro@gmail.com>
2023-02-07 00:36:34 +0600

    Result page works

--- src/components/Pages/Result.js
+++ src/components/Pages/Result.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import { useLocation, useParams } from 'react-router-dom';
 import Analysis from '../Analysis';
 import useAnswers from '../Hooks/useAnswers';
@@ -11,14 +12,42 @@ export default function Result() {
 	const { loading, error, answers } = useAnswers(id);
 	console.log(answers);
 
+	function scoreCalculate() {
+		let score = 0;
+
+		answers.forEach((question, questionIndex) => {
+			let correctIndexes = [],
+				checkedIndexes = [];
+
+			question.options.forEach((option, optionIndex) => {
+				if (option.correct) correctIndexes.push(optionIndex);
+				if (qna[questionIndex].options[optionIndex].checked) {
+					checkedIndexes.push(optionIndex);
+					option.checked = true;
+				}
+			});
+
+			if (_.isEqual(correctIndexes, checkedIndexes)) {
+				score = score + 5;
+			}
+		});
+
+		return score;
+	}
+
+	const userScore = scoreCalculate();
+
 	return (
 		<>
 			{loading && <span className="success">Loading...</span>}
 			{error && <span className="error">There was an error!</span>}
-			<>
-				<Summary />
-				<Analysis />
-			</>
+
+			{answers && answers.length > 0 && (
+				<>
+					<Summary score={userScore} noq={answers.length} />
+					<Analysis answers={answers} />
+				</>
+			)}
 		</>
 	);
 }
