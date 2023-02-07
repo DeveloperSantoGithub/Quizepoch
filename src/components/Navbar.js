6c0391f08ab722270d841a09c2f7c5f242727baf
Saidul Islam Santo <sksanto.pro@gmail.com>
2023-02-08 01:28:41 +0600

    add favicon and logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import logo from '../Assets/images/quiz-logo.png';
+import logo from '../Assets/images/logo.png';
 import classes from '../Styles/Navbar.module.css';
 import Account from './Account';
 export default function Navbar() {
