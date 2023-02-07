30ef8906da70a8492979d36f38020aac07b71441
Saidul Islam Santo <sksanto.pro@gmail.com>
2023-02-07 11:54:14 +0600

    Miniplayer functions added

--- src/components/MiniPlayer.js
+++ src/components/MiniPlayer.js
@@ -1,13 +1,39 @@
+import { useRef, useState } from 'react';
 import img from '../Assets/images/3.jpg';
 import classes from '../Styles/MiniPlayer.module.css';
 
 export default function MiniPlayer() {
+	const [status, setStatus] = useState(false);
+
+	const buttonRef = useRef();
+
+	function toggleMiniPlayer() {
+		if (!status) {
+			buttonRef.current.classList.remove(classes.floatingBtn);
+			setStatus(true);
+		} else {
+			buttonRef.current.classList.add(classes.floatingBtn);
+			setStatus(false);
+		}
+	}
+
 	return (
-		<div className={`${classes.miniPlayer} ${classes.floatingBtn}`}>
-			<span className={`material-icons-outlined ${classes.open}`}>
+		<div
+			className={`${classes.miniPlayer} ${classes.floatingBtn}`}
+			ref={buttonRef}
+		>
+			<span
+				className={`material-icons-outlined ${classes.open}`}
+				onClick={toggleMiniPlayer}
+			>
 				play_circle_filled
 			</span>
-			<span className={`material-icons-outlined ${classes.close}`}>close</span>
+			<span
+				className={`material-icons-outlined ${classes.close}`}
+				onClick={toggleMiniPlayer}
+			>
+				close
+			</span>
 			<img src={img} alt="Mini player" />
 			<p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
 		</div>
